7d8fb4b397a45144c8644f1932f393fafb1784de
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-01-12 09:56:37 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# EECS 337: Golden Globes
+
+# 1. Find the names of the hosts
+# 2. For each award, find the name of the winner 
+# 3. For each award, find the name of the presenter(s)
+# 4. Add something special to it
+  
