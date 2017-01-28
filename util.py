09ded0f93280660c8359441bc502c25f14eb50c4
cnakos <ConstantineNakos2020@u.northwestern.edu>
2017-01-28 12:12:55 -0600

    Updated util.py.

--- util.py
+++ util.py
@@ -11,8 +11,9 @@ USERPAT = re.compile('@.+')
 
 def is_special(tok):
 	"Return True if tok is a capitalized word, hashtag, user, or stopword."
-	return tok[0].isupper() or bool(HASHPAT.match(tok)) or \
-			bool(USERPAT.match(tok)) or tok.lower() in STOPWORDS
+	return (tok[0].isupper() and tok[1:].islower() and tok.lower() not in STOPWORDS) or \
+			bool(HASHPAT.match(tok)) or bool(USERPAT.match(tok)) #or \
+			#tok.lower() in STOPWORDS
 
 def find_special(toks):
 	"Return a list of lists of words selected by is_special()."
@@ -27,6 +28,16 @@ def find_special(toks):
 		else:
 			make_new = True
 	return specials
+
+SUPERLATIVES = ['most', 'least', 'best', 'worst', 'favorite']
+def find_superlatives(tweets):
+	supers = []
+	for tweet in tweets:
+		specials = find_special(tweet)
+		for special in specials:
+			if special[0].lower() in SUPERLATIVES:
+				supers.append(special)
+	return supers
 
 def tokenize(text):
 	return twokenize.tokenizeRawTweetText(text)
