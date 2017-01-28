aea4c0227719162878e7e3885698860d73f74b5f
Steven <stevenzmanx@u.northwestern.edu>
2017-01-28 12:09:03 -0600

    Added naive search for tweets using keywords

--- main.py
+++ main.py
@@ -4,4 +4,28 @@
 # 2. For each award, find the name of the winner 
 # 3. For each award, find the name of the presenter(s)
 # 4. Add something special to it
-  
+ 
+import re
+import tweet_reader as tr
+
+
+def naiveSearch(tweets, triggers):
+  	"""
+		Inputs: List of List of tweets , list of keywords
+  	"""
+	matchingTweets = []
+
+	for tweet in tweets:
+		for key in triggers:
+			if findWholeWord(key)(tweet[0]):
+				matchingTweets.append(tweet[0])
+
+	return matchingTweets
+
+def findWholeWord(w):
+	return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+
+tweets = tr.read_tweet_file("goldenglobes.tab")
+
+print naiveSearch(tweets, ["award", "Award"])
